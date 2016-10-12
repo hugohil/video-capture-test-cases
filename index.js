35408a22bb87f7833ba1afd19f39e1a74184cec2
hugohil <hugo@soixantecircuits.fr>
2016-10-12 16:22:20 +0200

    Add MediaRecorder API case

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 'use strict'
 
 const webcam = require('./node_modules/webcamjs/webcam.min.js')
+let mediaRecorder = null
 
 function recordViaMediaRecorder () {
+  document.querySelector('#target').setAttribute('src', '')
   console.log('recording a 5s video via MediaRecorder.')
+  let mediaChunks = []
+  mediaRecorder.start()
+  mediaRecorder.ondataavailable = (event) => {
+    ;(event.data.size > 0) && mediaChunks.push(event.data)
+  }
+  mediaRecorder.onstop = (event) => {
+    const path = `/tmp/video-media-${new Date().getTime()}.webm`
+    const blob = new Blob(mediaChunks, { type: 'video/webm' })
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(blob)
+    reader.addEventListener('load', function () {
+      const mediaDatas = reader.result
+      const mediaBuffer = new Buffer(mediaDatas)
+      require('fs').writeFile(path, mediaBuffer, (err) => {
+        if (err) {
+          throw err
+        } else {
+          document.querySelector('#target').setAttribute('src', `file://${path}`)
+        }
+      })
+    }, false)
+    reader.addEventListener('error', function (err) {
+      console.log(error)
+    })
+    mediaChunks = []
+  }
+  setTimeout(() => {
+    mediaRecorder.stop()
+  }, 5000)
 }
 
 function recordViaCcapture () {
@@ -14,10 +45,13 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM is loaded.')
 
   webcam.set({
-    width: 800,
-    height: 600
+    width: 400,
+    height: 300
   })
   webcam.attach('#source')
+  webcam.on('live', () => {
+    mediaRecorder = new MediaRecorder(webcam.stream, { mimeType: 'video/webm' })
+  })
 
   document.querySelector('.js-capture-media-recorder').addEventListener('click', recordViaMediaRecorder)
   document.querySelector('.js-capture-ccapture').addEventListener('click', recordViaCcapture)
